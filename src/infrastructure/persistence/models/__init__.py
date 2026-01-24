ce3043b273115aa06578b57dde07ca796946060a
Daniil Neudakhin <neudakhin.perm@gmail.com>
2026-01-24 20:39:25 +0500

    base and user models

---
+++ src/infrastructure/persistence/models/__init__.py
@@ -0,0 +1,4 @@
+from infrastructure.persistence.models.base import BaseModel
+from infrastructure.persistence.models.user import User
+
+__all__ = ["BaseModel", "User"]
---
+++ src/infrastructure/persistence/models/user.py
@@ -0,0 +1,18 @@
+import uuid
+
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from infrastructure.persistence.models import BaseModel
+
+
+class User(BaseModel):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True)
+    user_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(255), nullable=False, unique=True
+    )
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
