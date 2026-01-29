baddf335e3902ddec1656eb4950da2f499445805
Daniil Neudakhin <neudakhin.perm@gmail.com>
2026-01-29 13:26:45 +0500

    rework the user entity, remove factory methods and tests for them.

--- src/domain/entity/user.py
+++ src/domain/entity/user.py
@@ -1,14 +1,8 @@
 import uuid
 
-from typing_extensions import Self
 
-from core.entity import Entity
-from core.exceptions import EmptyStringError
-from core.service import AbstractHasher
-
-
-class User(Entity):
-    __slots__ = ("user_name", "email", "password")
+class User:
+    __slots__ = ("id", "user_name", "email", "password")
 
     def __init__(
         self,
@@ -17,37 +11,7 @@ class User(Entity):
         hashed_password: str | None = None,
         id: uuid.UUID | None = None,
     ) -> None:
-        super().__init__(id)
+        self.id: uuid.UUID = id if id else uuid.uuid4()
         self.user_name: str = user_name
         self.email: str = email
         self.password: str | None = hashed_password
-
-    @classmethod
-    def create(
-        cls,
-        user_name: str,
-        email: str,
-        password: str,
-        hasher: AbstractHasher,
-    ) -> Self:
-        if not password:
-            raise EmptyStringError("Password string can't be empty")
-        hashed_password = hasher.hash(password)
-        return cls(
-            user_name=user_name, email=email, hashed_password=hashed_password
-        )
-
-    @classmethod
-    def from_persistence_storage(
-        cls,
-        id: uuid.UUID,
-        user_name: str,
-        email: str,
-        hashed_password: str,
-    ) -> Self:
-        return cls(
-            id=id,
-            user_name=user_name,
-            email=email,
-            hashed_password=hashed_password,
-        )
