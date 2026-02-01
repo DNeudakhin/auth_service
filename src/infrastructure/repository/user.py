c9bcfb1018220f3613352392d4bbedeefcd4d110
Daniil Neudakhin <neudakhin.perm@gmail.com>
2026-02-01 13:56:20 +0500

    add new get method in repository and inject db manager dependency

--- src/infrastructure/repository/user.py
+++ src/infrastructure/repository/user.py
@@ -4,15 +4,27 @@ from sqlalchemy import select
 
 from domain import entity
 from infrastructure.persistence import model
-from infrastructure.persistence.manager import DBManager, manager
+from infrastructure.persistence.manager import DBManager
 from infrastructure.persistence.mapper import UserMapper
 
 
 class SqlAlchemyUserRepository:
     __slots__ = "_manager"
 
-    def __init__(self) -> None:
+    def __init__(self, manager: DBManager) -> None:
         self._manager: DBManager = manager
+
+    async def get_user_by_id(self, id: uuid.UUID) -> entity.User | None:
+        async with self._manager.session_maker() as s:
+            result = await s.execute(
+                select(model.User).where(model.User.id == id)
+            )
+            user = result.scalar_one_or_none()
+
+        if user:
+            return UserMapper.to_domain(user)
+
+        return None
 
     async def get_user_by_email(self, email: str) -> entity.User | None:
         async with self._manager.session_maker() as s:
